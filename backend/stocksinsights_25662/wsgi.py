0011fd0ee2bbb8144639fe8221dbbebfc47c650e
Crowdbotics <team@crowdbotics.com>
2021-04-13 14:09:00 +0000

    Initial commit.

---
+++ backend/stocksinsights_25662/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for stocksinsights_25662 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stocksinsights_25662.settings')
+
+application = get_wsgi_application()
